0d69917724d9a64cecbf53ea5e9c142d186a21bb
Thuy Tien Nguyen <35412770+juniHub@users.noreply.github.com>
2021-05-25 05:03:53 -0700

    a little change in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,8 +4,8 @@ function Footer() {
  
     return (
           
-    <div className="footer">
-        <p>Made with <span role="img" aria-label="heart">💖</span>by juniHUB@2020</p>
+     <div className="footer">
+        <p>Made with <span role="img" aria-label="heart">💖</span>by <a target="_blank" aria-label="Juni Portfolio" rel="noopener noreferrer"href="https://hellojuninguyen.netlify.app/">juniHUB</a> 2020</p>
     </div>
     )
 }
